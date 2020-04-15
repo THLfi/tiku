28e93ab9fb3887804193112f035435164f90d6d3
jmak <jani.malminen@thl.fi>
2020-04-15 14:17:01 +0300

    Removed one logging statement.
    Change-Id: I37cf15ab7225ad63961536fd2dc3e64d000caba5

--- src/main/java/fi/thl/pivot/web/CsrfController.java
+++ src/main/java/fi/thl/pivot/web/CsrfController.java
@@ -3,7 +3,6 @@ package fi.thl.pivot.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class CsrfController extends AbstractController {
 
-    private static final Logger LOG = Logger.getLogger(CsrfController.class);
-
     /**
      * End point to get csrf token bound to current session. Adds support for api-login from internet.
      * @return csrf token bound to current session.
@@ -21,7 +18,6 @@ public class CsrfController extends AbstractController {
     @RequestMapping(value = "/csrf", produces = "text/plain", method = RequestMethod.GET)
     @ResponseBody
     public String getCsrf(HttpSession session, HttpServletRequest request) {
-        LOG.debug(String.format("RETRIEVE CSRF OF SESSION %s [%s]", session.getId(), request.getRemoteAddr()));
         return getCsrf();
     }
 
