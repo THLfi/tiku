bd065b61127690ecbbba2b938f8737004e5f476e
jmak <jani.malminen@thl.fi>
2020-04-20 09:38:03 +0300

    Fixed incorrect array manipulation of map color palette
    Palette array was mutated, which broke coloring of summaries having more
    than one map.
    Change-Id: I679558c95798cc57f9a442de7656647c72359163

--- src/main/webapp/resources/js/map-palette.js
+++ src/main/webapp/resources/js/map-palette.js
@@ -1,21 +1,22 @@
 // kaksisuuntainen
 var rankingPalettes = {
-	9: ['#2F62AD','#387fb5','#6f9fc4','#a5bfd3','#DCDFE2','#d5b7c6','#ce8fab','#c6678f','#BE3F72'],
-	8: ['#2F62AD','#387fb5','#6f9fc4','#a5bfd3','#d5b7c6','#ce8fab','#c6678f','#BE3F72'],
-	7: ['#2F62AD','#387fb5','#a5bfd3','#DCDFE2','#d5b7c6','#ce8fab','#c6678f'],
-	6: ['#2F62AD','#387fb5','#a5bfd3','#d5b7c6','#c6678f','#BE3F72'],
-	5: ['#2F62AD','#6f9fc4','#DCDFE2','#ce8fab','#BE3F72'],
-	4: ['#2F62AD','#6f9fc4','#ce8fab','#BE3F72'],
-	3: ['#2F62AD','#DCDFE2','#BE3F72']
+	9: ['#BE3F72','#c6678f','#ce8fab','#d5b7c6','#DCDFE2','#a5bfd3','#6f9fc4','#387fb5','#2F62AD'],
+	8: ['#BE3F72','#c6678f','#ce8fab','#d5b7c6','#a5bfd3','#6f9fc4','#387fb5','#2F62AD'],
+	7: ['#c6678f','#ce8fab','#d5b7c6','#DCDFE2','#a5bfd3','#387fb5','#2F62AD'],
+	6: ['#BE3F72','#c6678f','#d5b7c6','#a5bfd3','#387fb5','#2F62AD'],
+	5: ['#BE3F72','#ce8fab','#DCDFE2','#6f9fc4','#2F62AD'],
+	4: ['#BE3F72','#ce8fab','#6f9fc4','#2F62AD'],
+	3: ['#BE3F72','#DCDFE2','#2F62AD']
+
 };
 
 // sarjallinen
 var monotonePalettes = {
-	7: ['#519B2F','#87B65C','#9FBF70','#B6C985','#CDD29A','#E5DBAE','#FCE4C3'],
-	6: ['#519B2F','#8cb860','#a8c379','#c4ce91','#e0d9aa','#fce4c3'],
-	5: ['#519B2F','#93bb66','#b6c985','#d9d6a4','#fce4c3'],
-	4: ['#519B2F','#9fbf70','#cdd29a','#fce4c3'],
-	3: ['#519B2F','#b6c985','#fce4c3']
+	7: ['#FCE4C3','#E5DBAE','#CDD29A','#B6C985','#9FBF70','#87B65C','#519B2F'],
+	6: ['#fce4c3','#e0d9aa','#c4ce91','#a8c379','#8cb860','#519B2F'],
+	5: ['#fce4c3','#d9d6a4','#b6c985','#93bb66','#519B2F'],
+	4: ['#fce4c3','#cdd29a','#9fbf70','#519B2F'],
+	3: ['#fce4c3','#b6c985','#519B2F']
 };
 
 var grayPalette = ['#b2b2b2', '#8c8c8c','#666666','#3f3f3f','#191919'];
@@ -35,13 +36,13 @@ mapPalette.createMapColors = function(paletteType, numberOfColors) {
 		if (numberOfColors > 9 || numberOfColors < 3) {
 			throw Error('Number of colors for palette "' + paletteType + '" has to be between 3-9.');
 		}
-		return rankingPalettes[numberOfColors].reverse();
+		return rankingPalettes[numberOfColors];
 	}
 	else if (paletteType === 'monotone') {
 		if (numberOfColors > 7 || numberOfColors < 3) {
 			throw Error('Number of colors for palette "' + paletteType + '" has to be between 3-7.');
 		}
-		return monotonePalettes[numberOfColors].reverse();
+		return monotonePalettes[numberOfColors];
 	}
 	else {
 		if (numberOfColors !== 5) {
